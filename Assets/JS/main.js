c3621c410dbe05b939d4508639bcf902fda20ef9
Samiur Rahman <srsami707@gmail.com>
2024-01-15 15:45:46 +0600

    TODO: fix due amount calculations tomorrow

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -4,43 +4,130 @@ $(function () {
     subtotalCalculator();
 });
 
+// PreventDefault on form submit
+$("#item-form").submit(function (e) {
+    return false;
+});
+
+
 // NOTE: This function is used to check if user gave discount or total payable amount and disable the other input field and calculate the discounted price
 function handleDiscount(option) {
+    // Get references to input elements
     var discountInput = document.getElementById('discount');
     var totalPayableInput = document.getElementById('total-payable');
     var subtotalInput = document.getElementById('subtotal');
+    var discountAmountInput = document.getElementById('discountAmount');
+    var duePaymentInput = document.getElementById('due-payment');
 
     if (option === 'discount') {
-        if (discountInput.value !== '' && parseFloat(discountInput.value) !== 0 && parseFloat(discountInput.value) <= 100 && parseFloat(discountInput.value) >= 0) {
-            let originalPrice = parseInt(subtotalInput.value);
-            var discountAmount = (parseInt(discountInput.value) / 100) * originalPrice;
-            var discountedPrice = originalPrice - discountAmount;
-            totalPayableInput.value = discountedPrice.toFixed(2);
+        // Get the discount value from the discount input
+        var discountValue = parseFloat(discountInput.value);
+
+        // Check if the discountValue is a valid number between 0 and 100
+        if (!isNaN(discountValue) && discountValue >= 0 && discountValue <= 100) {
+            // Get the original price from the subtotal input
+            var originalPrice = parseFloat(subtotalInput.value);
+
+            // Calculate the discount amount and update the input
+            var discountAmount = (discountValue / 100) * originalPrice;
+            discountAmountInput.value = discountAmount.toFixed(2);
+
+            // Calculate the total payable price and update the input
+            totalPayableInput.value = (originalPrice - discountAmount).toFixed(2);
+
+            // Disable the discount amount and total payable inputs
+            discountAmountInput.disabled = true;
             totalPayableInput.disabled = true;
         } else {
+            // Clear input fields and enable them
             discountInput.value = '';
             totalPayableInput.value = '';
+            discountAmountInput.value = '';
+
+
+            discountAmountInput.disabled = false;
+            totalPayableInput.disabled = false;
+        }
+    } else if (option === 'discountAmount') {
+        // Get the discount amount from the discountAmount input
+        var discountAmount = parseFloat(discountAmountInput.value);
+
+        // Check if the discountAmount is a valid number between 0 and the subtotal
+        if (!isNaN(discountAmount) && discountAmount >= 0 && discountAmount <= parseFloat(subtotalInput.value)) {
+            // Get the original price from the subtotal input
+            var originalPrice = parseFloat(subtotalInput.value);
+
+            // Calculate the discount percentage and update the discount input
+            var discountPercentage = (discountAmount / originalPrice) * 100;
+            discountInput.value = discountPercentage.toFixed(2);
+
+            // Calculate the total payable price and update the input
+            totalPayableInput.value = (originalPrice - discountAmount).toFixed(2);
+
+            // Disable the discount input and total payable inputs
+            discountInput.disabled = true;
+            totalPayableInput.disabled = true;
+        } else {
+            // Clear input fields and enable them
+            discountAmountInput.value = '';
+            discountInput.value = '';
+            totalPayableInput.value = '';
+
+            discountInput.disabled = false;
             totalPayableInput.disabled = false;
         }
     } else if (option === 'total-payable') {
-        if (totalPayableInput.value !== '' && parseFloat(totalPayableInput.value) !== 0 && parseFloat(totalPayableInput.value) <= parseFloat(subtotalInput.value) && parseFloat(totalPayableInput.value) >= 0) {
-            let originalPrice = parseFloat(subtotalInput.value);
-            var discountValue = parseFloat(totalPayableInput.value);
+        // Get the total payable value from the totalPayableInput
+        var totalPayableValue = parseFloat(totalPayableInput.value);
 
-            // Convert discount value to percentage
-            var discountPercentage = (discountValue / originalPrice) * 100;
+        // Check if the totalPayableValue is a valid number between 0 and the subtotal
+        if (!isNaN(totalPayableValue) && totalPayableValue >= 0 && totalPayableValue <= parseFloat(subtotalInput.value)) {
+            // Get the original price from the subtotal input
+            var originalPrice = parseFloat(subtotalInput.value);
 
-            // Calculate discounted price
-            var discountedPrice = discountValue;
+            // Calculate the discount percentage based on the total payable and update the discount input
+            var discountPercentage = ((originalPrice - totalPayableValue) / originalPrice) * 100;
             discountInput.value = discountPercentage.toFixed(2);
+
+            // Disable the discount input
             discountInput.disabled = true;
         } else {
+            // Clear input fields and enable them
             totalPayableInput.value = '';
             discountInput.value = '';
+            discountAmountInput.value = '';
+
             discountInput.disabled = false;
+            discountAmountInput.disabled = false;
+        }
+    } else if (option === 'due-payment') {
+        // Get the due payment value from the duePaymentInput
+        var totalPayableValue = parseFloat(totalPayableInput.value);
+        var duePaymentInput = document.getElementById('due-payment');
+
+        // Check if the duePaymentValue is a valid number between 0 and the subtotal
+        if (!isNaN(totalPayableValue) && totalPayableValue >= 0 && totalPayableValue <= parseFloat(subtotalInput.value)) {
+            // Get the original price from the subtotal input
+            var originalPrice = totalPayableValue;
+
+            // Calculate the total payable price and update the input
+            duePaymentInput.value = (originalPrice - duePaymentValue).toFixed(2);
+
+            // Disable the total payable input
+            totalPayableInput.disabled = true;
+        } else {
+            // Clear input fields and enable them
+            totalPayableInput.value = '';
+
+            totalPayableInput.disabled = false;
         }
     }
 }
+
+function duePaymentCalculator() {
+
+}
+
 
 // Usage example:
 // For discount input change
@@ -59,8 +146,7 @@ function subtotalCalculator() {
         sum += parseInt(prices[i].value) || 0; // Ensure to add 0 if the value is not a valid number
     }
 
-    document.getElementById('subtotal').value = sum;
-
+    document.getElementById('subtotal').value = sum.toFixed(2);
 }
 
 // Enable/Disable the item-price input field
@@ -126,48 +212,35 @@ window.addEventListener("load", function () {
 // TODO: Fix the css
 function insRow(item_name, item_description, item_price) {
     var table = document.getElementById('item-table');
-
     var x = table.rows.length;
-    var id = "tbl" + x;
+
     var row = table.insertRow(x);
+    var id = "tbl" + x;
     row.id = id;
+
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    cell1.outerHTML = `<td style="width: 250px;">
-    <input class="form-control" id="formType" disabled value="` + item_name + `"></td>`;
-    cell2.innerHTML = '<td><textarea type="text" disabled class="form-control" style="height: 2px;" id="item-description" placeholder="Item Description" required>' + item_description + '</textarea></td>';
-    cell3.innerHTML = `<td>
-    <div class="input-group mb-3">
-        <span class="input-group-text">BDT</span>
-        <input type="number" class="form-control" style="text-align: right;" onchange="subtotalCalculator()"
-            id="item-price" value="` + item_price + `" disabled aria-label="Item Quantity">
-        <span class="input-group-text">৳</span>
-    </div>
-</td>`;
-    cell4.innerHTML = `<td>
-    <button class="btn btn-danger btn-sm" onclick="deleteRow(this)"><span
-            class="fa fa-trash"></span></button>
-    <button class="btn btn-success btn-sm" onclick="subtotalCalculator('new'); insRow()"><span
-            class="fa fa-plus"></span></button>
-</td>`;
+
+    cell1.innerHTML = '<td><input style="min-width: 175px;" class="form-control" id="formType" disabled value="' + item_name + '"></td>';
+    cell2.innerHTML = '<td><textarea type="text" disabled class="form-control" style="min-width: 355px; height: 2px;" id="item-description" placeholder="Item Description" required>' + item_description + '</textarea></td>';
+    cell3.innerHTML = '<td><div style="min-width: 190px;" class="input-group mb-3" id="item-price-container"><span class="input-group-text">BDT</span><input type="number" class="form-control" style="text-align: right;" id="item-price" oninput="subtotalCalculator()" value="' + item_price + '" disabled aria-label="Item Quantity"><span class="input-group-text">৳</span></div></td>';
+    cell4.innerHTML = '<td><div style="min-width: max-content;"><button class="btn btn-outline-primary" onclick="insRow()" style="margin-right: 5px;"><span class="fa fa-pencil"></span></button><button class="btn btn-danger" onclick="deleteRow(this); subtotalCalculator()"><span class="fa fa-trash"></span></button></div></td>';
+
     subtotalCalculator();
     clearModal('item');
-    x.appendChild(new_row);
 }
 
 
 // Delete Row
 // Additional: Don't delete the first row
-function deleteRow(row) {
-    var i = row.parentNode.parentNode.rowIndex;
-    if (i == 1) {
-        return;
-    } else {
-        document.getElementById('item-table').deleteRow(i);
+function deleteRow(button) {
+    var row = button.parentNode.parentNode.parentNode;
+    if (row.rowIndex > 1) {
+        document.getElementById('item-table').deleteRow(row.rowIndex);
+        subtotalCalculator();
     }
-    subtotalCalculator();
 }
 
 // ================================

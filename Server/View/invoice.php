045945849196a913f9714d9f0d5764199cd91c2e
Sami Rahman <srsami707@gmail.com>
2024-01-27 11:54:13 +0600

    Delete Backup Files

--- Server/View/invoice.php
+++ Server/View/invoice.php
@@ -379,9 +379,9 @@ $receipt_id = $receipt->generateReceiptID();
                 <!-- Container wrapper -->
                 <div class="container-fluid">
                     <!-- Toggle button -->
-                    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <!-- <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-bars"></i>
-                    </button>
+                    </button> -->
 
                     <!-- Brand -->
                     <a class="navbar-brand" href="#" style="position: fixed; left: 20px; top: 10px;">

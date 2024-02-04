11abf75e305fcc925706f2c9bd4223d74c994387
Sami Rahman <srsami707@gmail.com>
2024-02-03 22:20:50 -0800

    Minor Bug Fixes [Working]

--- Public/autoloader.php
+++ Public/autoloader.php
@@ -1,8 +1,24 @@
 <?php
 
 spl_autoload_register(function ($className) {
-    // Define the base directory for your classes
-    $baseDir = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+    /**
+     * TITLE: SET PUBLISH MODE [true/false][Server/Localhost]
+     * ~ Description: Set the publish mode to true or false
+     * 
+     * @var string $publish
+     * @var string $baseDir
+     * @var string $classFile
+     */
+    // ----------------------------------------
+    $publish = "false";
+    // ----------------------------------------
+
+    // Check If Published Mode True/False
+    if ($publish == "false") {
+        $baseDir = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'Invoicing-System' . DIRECTORY_SEPARATOR; // Localhost Directory
+    } else {
+        $baseDir = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR; // Server Directory
+    }
 
     // Normalize the class name (replace namespace separators with directory separators)
     $classFile = $baseDir . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
--- Public/index.php
+++ Public/index.php
@@ -1,13 +1,31 @@
 <?php
 
-session_start();
+/**
+ * TITLE: SET PUBLISH MODE [true/false][Server/Localhost]
+ * ~ Description: Set the publish mode to true or false
+ * 
+ * @var string $publish
+ * @var string $server_addr
+ */
+$publish = "false"; // Set to "true" to publish
+
+session_start(); // Start the session
+
+if ($publish === "false") {
+    $server_addr = "../Server/"; // Localhost Server directory
+} else {
+    $server_addr = "../../Server/"; // Public Server directory
+}
+
+// Include the autoloader
 require_once "autoloader.php";
+
 if (isset($_SESSION['agent']['loggedin'])) {
     if ($_SESSION['agent']['loggedin']) {
-        include_once "../Server/View/invoice.php";
+        include_once $server_addr . "View/invoice.php";
     } else {
-        include_once "../Server/View/login.php";
+        include_once $server_addr . "View/login.php";
     }
 } else {
-    include_once "../Server/View/login.php";
+    include_once $server_addr . "View/login.php";
 }
